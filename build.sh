6a1eda6dc515cc00c0c6a11bd166fd0be1a6e5d7
Wenbing Li <wenbing.li@disney.com>
2023-02-08 20:43:24 -0800

    Fix build script that `sed` need argument for -i option

--- build.sh
+++ build.sh
@@ -11,12 +11,20 @@ mkdir -p ../dist/v${VERSION}
 zip -r ../dist/v${VERSION}/chrome-chatgpt-share-v${VERSION}.zip .
 
 # replace manifest version from 3 to 2
-sed -i 's/"manifest_version": 3/"manifest_version": 2/g' manifest.json
+if [ "$(uname)" == "Darwin" ]; then
+  sed -i "" 's/"manifest_version": 3/"manifest_version": 2/g' manifest.json
+else
+  sed -i 's/"manifest_version": 3/"manifest_version": 2/g' manifest.json
+fi
 
 # build firefox
 zip -r ../dist/v${VERSION}/firefox-chatgpt-share-v${VERSION}.zip .
 
 # restore manifest version from 2 to 3
-sed -i 's/"manifest_version": 2/"manifest_version": 3/g' manifest.json
+if [ "$(uname)" == "Darwin" ]; then
+  sed -i "" 's/"manifest_version": 2/"manifest_version": 3/g' manifest.json
+else
+  sed -i 's/"manifest_version": 2/"manifest_version": 3/g' manifest.json
+fi
 
 cd ..
